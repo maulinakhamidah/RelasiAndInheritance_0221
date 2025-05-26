8c0572749dba3cf4d7aa94ea69c512f2b00e7734
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-26 23:46:37 +0700

    add void function pada dokter

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -4,3 +4,50 @@ using namespace std;
 
 class dokter;
 class pasien
+{ // aosiasi
+public :
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama)
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien()
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter
+{
+public :
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama)
+    {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter()
+    {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter)
+{
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter()
+{
+    cout << "Daftar DOkter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter)
+    { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diitera
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
